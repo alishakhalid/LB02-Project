e0d9b06d734e5bb51630366027cee4bc827f8c34
alishakhalid <69908299+alishakhalid@users.noreply.github.com>
2022-06-25 01:42:56 +0200

    navigation done

--- frontend/kalyani/src/App.tsx
+++ frontend/kalyani/src/App.tsx
@@ -2,9 +2,18 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Registrate from "./components/pages/Registration";
-
-function App() {
-  return <Registrate />;
-}
+import { CssBaseline } from "@mui/material";
+import { MuiThemeProvider } from "@material-ui/core";
+import theme from "./config/Theme";
+import Navigate from "./Routing/Navigation";
+const App = () => {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline>
+        <Navigate />
+      </CssBaseline>
+    </MuiThemeProvider>
+  );
+};
 
 export default App;
--- frontend/kalyani/src/Routing/Navigation.tsx
+++ frontend/kalyani/src/Routing/Navigation.tsx
@@ -1,27 +1,26 @@
-import { render } from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "../App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Registrate from "../components/pages/Registration";
+import Checkout from "../components/pages/CheckoutPage";
 import Homepage from "../components/pages/Homepage";
 import Login from "../components/pages/Login";
 import MultiItemPage from "../components/pages/MultiItemPage";
-import Registrate from "../components/pages/Registration";
 import SingleItemPage from "../components/pages/SingleItemPage";
-// import your route components too
-const Router = () => {
+import Order from "../components/pages/Order";
+import App from "../App";
+
+export default function Navigate() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Homepage />} />
-          <Route path="registrate" element={<Registrate />}>
-            <Route path="login:userId" element={<Login />} />
-            <Route path="multipage" element={<MultiItemPage />} />
-            <Route path="singlepage" element={<SingleItemPage />} />
-          </Route>
-        </Route>
+        <Route path={"/"} element={<App />} />
+        <Route path={"/home"} element={<Homepage />} />
+        <Route path={"/clothes"} element={<MultiItemPage />} />
+        <Route path={"/clothes/:id"} element={<SingleItemPage />} />
+        <Route path={"/checkout"} element={<Checkout />} />
+        <Route path={"/order"} element={<Order />} />
+        <Route path={"/registrate"} element={<Registrate />} />
+        <Route path={"/login"} element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
-};
-
-export default Router;
+}
